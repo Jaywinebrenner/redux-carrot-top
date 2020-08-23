8411cdc2bf860829e9c4a278282452cb1c3abff2
jay winebrenner <jaywinebrenner@jays-iMac-2.local>
2020-08-23 14:52:25 -0700

    Updates

---
+++ precache-manifest.ddebbef29712d24c391d58476d647825.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb2ad436611eabb854ff0e6f3f8c09a",
+    "url": "/redux-carrot-top.git/index.html"
+  },
+  {
+    "revision": "497ce4e45104c1da2834",
+    "url": "/redux-carrot-top.git/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "1f2ad33b95153a492d93",
+    "url": "/redux-carrot-top.git/static/css/main.15bb6995.chunk.css"
+  },
+  {
+    "revision": "497ce4e45104c1da2834",
+    "url": "/redux-carrot-top.git/static/js/2.2a9b2a43.chunk.js"
+  },
+  {
+    "revision": "f828ba3f58479656d16ef621c409174d",
+    "url": "/redux-carrot-top.git/static/js/2.2a9b2a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2ad33b95153a492d93",
+    "url": "/redux-carrot-top.git/static/js/main.41b1fa12.chunk.js"
+  },
+  {
+    "revision": "54389298a607b2c6b1af",
+    "url": "/redux-carrot-top.git/static/js/runtime-main.d2cb6a79.js"
+  },
+  {
+    "revision": "8f0c4ba2ea1a4a1c30cb5e8af6e19417",
+    "url": "/redux-carrot-top.git/static/media/battle.8f0c4ba2.mp3"
+  },
+  {
+    "revision": "83b1565f1ae6730e5be8d9a963430a0a",
+    "url": "/redux-carrot-top.git/static/media/blood_pix.83b1565f.png"
+  },
+  {
+    "revision": "54b61746d3c7584e22dfe25cfb3ab7ce",
+    "url": "/redux-carrot-top.git/static/media/carrot-top.54b61746.png"
+  },
+  {
+    "revision": "1250a54633684286e75defad157ae150",
+    "url": "/redux-carrot-top.git/static/media/dice.1250a546.jpg"
+  },
+  {
+    "revision": "9230313d3e97bc86def0e2ca8103da3e",
+    "url": "/redux-carrot-top.git/static/media/emo.9230313d.png"
+  },
+  {
+    "revision": "fdd824841064e4eabe81dfd6529bf7ec",
+    "url": "/redux-carrot-top.git/static/media/emo.fdd82484.jpg"
+  },
+  {
+    "revision": "712d1a288ea705dc3860a53eb3ed91a4",
+    "url": "/redux-carrot-top.git/static/media/gallagher.712d1a28.jpg"
+  },
+  {
+    "revision": "2757140bfa1d8fd07d1803acbf7610b4",
+    "url": "/redux-carrot-top.git/static/media/gameplay.2757140b.mp3"
+  },
+  {
+    "revision": "44dae754a223478a063239f4334d2c16",
+    "url": "/redux-carrot-top.git/static/media/judy.44dae754.jpg"
+  },
+  {
+    "revision": "e430585ae7ce3ef8eb04d14c493e510c",
+    "url": "/redux-carrot-top.git/static/media/kinison.e430585a.jpg"
+  },
+  {
+    "revision": "c9694bf58b8330557e759a1948b4ad33",
+    "url": "/redux-carrot-top.git/static/media/rodney.c9694bf5.jpg"
+  },
+  {
+    "revision": "02380444df28c1bf2724a0e1a9748fd8",
+    "url": "/redux-carrot-top.git/static/media/theme1.02380444.mp3"
+  },
+  {
+    "revision": "579aa1ad9506771e53a586998e5f7dbf",
+    "url": "/redux-carrot-top.git/static/media/themeAbridged.579aa1ad.mp3"
+  },
+  {
+    "revision": "7a6a4b5eec066e1e0c93f4af7103053f",
+    "url": "/redux-carrot-top.git/static/media/tim.7a6a4b5e.jpg"
+  },
+  {
+    "revision": "edeca76d853ceadee84291b3cc5b08db",
+    "url": "/redux-carrot-top.git/static/media/victoria.edeca76d.jpg"
+  },
+  {
+    "revision": "6e5e8c4f35e785141d19903da95c4f73",
+    "url": "/redux-carrot-top.git/static/media/yakov.6e5e8c4f.jpg"
+  }
+]);
